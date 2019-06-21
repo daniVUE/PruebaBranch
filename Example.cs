f4fdbe54da2f00cdffbbf0daf647e853d0827ec7
Dani <dani2trrs91@hotmail.com>
2019-06-21 15:57:31 +0200

    First Commit

---
+++ Example.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploEnum
+{
+    class Program
+    {
+       
+
+        static void Main()
+        {
+            // ... An enum local variable.
+            Importance value = Importance.Critical;
+            
+            //if fuertemente tipado.
+            // ... Test against known Importance values.
+            if (value == Importance.Trivial)
+            {
+                Console.WriteLine("Not true");
+            }
+            else if (value == Importance.Critical)
+            {
+                Console.WriteLine("True");
+            }
+        }
+    }
+
+}
